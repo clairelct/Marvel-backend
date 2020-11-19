5c1378759d3fd1e8e47dd4523a6f9ab273906557
Claire Leconte <claire.lcnt@gmail.com>
2020-11-19 19:46:51 +0100

    Add route /characters/:id/comics

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const md5 = require("md5");
-const uid2 = require("uid2");
 
 const app = express();
 app.use(cors());
 
-// API Key Marvel
-const api_public_key = process.env.MARVEL_API_PUBLIC_KEY;
-const api_private_key = process.env.MARVEL_API_PRIVATE_KEY;
+// Import des routes
+const charactersRoutes = require("./routes/characters");
+app.use(charactersRoutes);
+const comicsRoutes = require("./routes/comics");
+app.use(comicsRoutes);
 
 app.get("/", async (req, res) => {
   res.status(200).json(response.data);
-});
-
-app.get("/characters", async (req, res) => {
-  try {
-    const ts = uid2(8);
-    const hash = md5(ts + api_private_key + api_public_key);
-
-    let limit = Number(req.query.limit);
-
-    const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/characters?limit=${limit}&ts=${ts}&apikey=${api_public_key}&hash=${hash}`
-    );
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
 });
 
 app.all("*", (req, res) => {
---
+++ routes/comics.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const md5 = require("md5");
+const uid2 = require("uid2");
+
+// API Key Marvel
+const api_public_key = process.env.MARVEL_API_PUBLIC_KEY;
+const api_private_key = process.env.MARVEL_API_PRIVATE_KEY;
+
+router.get("/comics", async (req, res) => {
+  try {
+    const ts = uid2(8);
+    const hash = md5(ts + api_private_key + api_public_key);
+
+    const { limit, offset } = req.query;
+
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/comics?limit=${limit}&offset=${offset}&ts=${ts}&apikey=${api_public_key}&hash=${hash}`
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
