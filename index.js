6b704f3b5a986430eb384e814ba1d276f1f2bdf8
Claire Leconte <claire.lcnt@gmail.com>
2020-11-22 12:07:20 +0100

    fix bug heroku deploy

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use(charactersRoutes);
 const comicsRoutes = require("./routes/comics");
 app.use(comicsRoutes);
 
-app.get("/", async (req, res) => {
-  res.status(200).json(response.data);
-});
+// app.get("/", async (req, res) => {
+//   res.status(200).json(response.data);
+// });
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "Page doesn't exists!" });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server started on Port 3001");
+  console.log(`Server started on Port ${process.env.PORT}`);
 });
